4b0fbd4e622bc42050fe0c61d227b66e77e673bc
BavithraSembagounder <bavithra.s2012@kgkite.ac.in>
2016-03-11 16:09:00 +0530

    Added files via upload

--- approval.php
+++ approval.php
@@ -1,19 +1,70 @@
+<!DOCTYPE html>
+
 <html>
 <head>
-<script>
-}(window.jQuery));
-$('#datepicker').datepicker({
-       /*  format: 'yyyy-mm-dd',
-        multidate: true,
-        multidateSeparator: ",",
-        calendarWeeks: true,
-        todayHighlight: true */
-    }).on('changeDate', function(e){
-        alert($('#datepicker1').datepicker('getDates'));
-    });
 
-</script>
-</head>
+	<link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
+	<script src="plugins/datepicker/bootstrap-datepicker.js"></script>
+<?php
+	$con=mysql_connect("localhost","root","");
+	mysql_select_db("dwa",$con);
+
+?>
+		<script>
+			function getXMLHTTP()
+			{
+			var xmlhttp=false;
+				try
+				{
+				xmlhttp= new XMLHttpRequest();
+				}
+				catch(e)
+				{
+					try
+					{
+					xmlhttp= new ActiveXObject("Microsoft.XMLHTTP");
+					}
+
+					catch(e)
+					{
+						try
+						{
+						xmlhttp=new ActiveXObject("Msxml2.XMLHTTP");
+						}
+						catch(e1)
+						{
+						xmlhttp=false;
+						}
+					}
+				}
+				return xmlhttp;
+			}
+
+			function attendance(val)
+			{
+				
+				var strURL="attendance.php?att="+val; 
+				
+				var req = getXMLHTTP();
+				if (req) 
+				{
+					req.onreadystatechange = function() 
+					{
+						if (req.readyState == 4) {
+							// only if "OK" 
+							if (req.status == 200) {	
+								document.getElementById('detail').innerHTML=req.responseText;					
+							} else {
+								alert("There was a problem while using XMLHTTP:\n" + req.statusText);
+							}
+						}				
+					}			
+					req.open("GET", strURL, true);
+					req.send(null);
+				}	
+			}
+		</script>
+		</head>
 <body>
 <?php include "head.php"; ?>
 	<!-- Content Wrapper. Contains page content -->
@@ -42,33 +93,35 @@ $('#datepicker').datepicker({
 						<!-- form start -->
 						<!--<form role="form">-->
 						<div class="box-body">
-
-								<table align="center">
-									<form name="book_approve" action="approval.php" method="post" onSubmit="return check(this)">
+									<form name="book_approve" action="approval.php" method="post" >
+									<table align="center">
 										<div>
 											<!--<tr><td><label><h4><font size="3" face="arial" color="red">*</font>Emp Id</h4></label></td><td><input type="text" size="35px" name="emp_id" id="emp_id" required></td></tr>-->
-											<tr><td><label><h4><font size="3" face="arial" color="red">*</font>Customer Name</h4></label></td><td><input type="text" size="35px" name="name" id="name" required></td>
-											<div class="datepicker" id="datepicker1" ><tr><td><label><h4><font size="3" face="arial" color="red">*</font>Required Date</h4></label></td><td><input type="date" size="35px" name="location" id="datepicker" required></td></tr></div>
+											<tr><td><label><h4><font size="3" face="arial" color="red">*</font>Customer Name</h4></label></td><td><input type="text" size="35px" name="name" id="name" required></td></tr>
+											<tr><td><label><h4><font size="3" face="arial" color="red">*</font>Required Date</h4></label></td><td><input type="date" size="35px" name="req_date" id="req_date" required></td>
 											<tr><td><label><h4><font size="3" face="arial" color="red">*</font>Status</h4></label></td><td><select name="status" id="sta" required>
 											<option>---Status---</option>
 											<option> Waiting for approval</option>
 											</select></td></tr>
-											<tr><td><label><h4><font size="3" face="arial" color="red">*</font>Select Person</h4></label></td><td><select name="person" id="person" required>
-											<?php
-											$sql=mysql_query("select * from attendance");
+											<tr><td><label><h4><font size="3" face="arial" color="red">*</font>Service person</h4></label></td>
+										<td><select name="name" onchange="attendance(this.value)">
+										<option value="">--Select Employee--</option>
+										<?php
+											$sql=mysql_query("select *  from workersdetail");
 											while($row=mysql_fetch_assoc($sql))
 											{
 												?>
-												<option value="<?php echo $row['id']; ?>"><?php echo $row['name'];?></option>
+												<option value="<?php echo $row['emp_id']; ?>"><?php echo $row['name']; ?></option>
 												<?php 
-											}
-											 ?>
-											</select></td></tr>
+											} 
+										?>
+									</select>
+										</td></tr>
 											<tr><td><label><h4><font size="3" face="arial" color="red">*</font>contact Number</h4></label></td><td><input type="text" size="35px" name="out_time" id="out_time" required></td></tr>
 											<tr><td><h4><center><input type="submit" name="submit" value="Submit"></h4></center></td>
 											<td><h4><center><input type="reset" name="reset" value="Reset"></h4></center></td></tr>
 											</table>
-								<table border=2" align="center">
+								<table id="example2" class="table table-bordered table-hover">
 <tr>
 <th bgcolor="66CCFF">Customer name</th>
 <th bgcolor="66CCFF">Contact Number</th>
@@ -91,6 +144,6 @@ $('#datepicker').datepicker({
 	</div>
 	<!-- /.box -->
 	<?php include "navigation.php"; ?>
-	<?php include "footer.php"; ?>
+	<?php include "footer.php"; ?>	
 </body>
 </html>
